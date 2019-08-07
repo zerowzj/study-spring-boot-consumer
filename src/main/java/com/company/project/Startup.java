b92723d54667574ea22a0ce6fd220506a4d6e965
wangzhenjun <wangzhenjun@okay.cn>
2019-08-07 11:11:34 +0800

    更新代码

--- src/main/java/com/company/project/Startup.java
+++ src/main/java/com/company/project/Startup.java
@@ -1,21 +1,16 @@
 package com.company.project;
 
+import com.company.project.support.config.SpringBootCfg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@EnableFeignClients
-@EnableDiscoveryClient
-@SpringBootApplication
 public class Startup {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Startup.class);
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Startup.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootCfg.class, args);
     }
 }
---
+++ src/main/java/com/company/project/support/config/SpringBootCfg.java
@@ -0,0 +1,11 @@
+package com.company.project.support.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringBootCfg {
+}
