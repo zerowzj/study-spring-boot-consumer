4ccc935cba26ccc54f5c339deea30d804f1fe920
wangzhenjun <wangzhenjun@okay.cn>
2019-08-07 14:10:17 +0800

    更新代码

--- src/main/java/com/company/project/controller/FeignController.java
+++ src/main/java/com/company/project/controller/FeignController.java
@@ -1,7 +1,24 @@
 package com.company.project.controller;
 
+import com.company.project.service.DemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/feign")
 public class FeignController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeignController.class);
+
+    @Autowired
+    private DemoService demoService;
+
+    @GetMapping("/sayHi")
+    public void sayHi() {
+        demoService.sayBye("wangzhj");
+    }
 }
--- src/main/java/com/company/project/controller/RibbonController.java
+++ src/main/java/com/company/project/controller/RibbonController.java
@@ -2,17 +2,20 @@ package com.company.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/ribbon")
 public class RibbonController {
 
-//    @Autowired
+    @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/sayHi")
     public void sayHi() {
-        restTemplate.getForObject("http://www.sohu.com", String.class);
+        String str = restTemplate.getForObject("http://www.sohu.com", String.class);
+        System.out.println(str);
     }
 }
