3020b50710198beef90277c2e744f591dfd27531
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-15 15:13:31 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/Startup.java
+++ src/main/java/study/springcloud/consumer/Startup.java
@@ -3,20 +3,13 @@ package study.springcloud.consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ConfigurableApplicationContext;
 import study.springcloud.consumer.support.SpringBootCfg;
 
-@EnableEurekaClient
-@EnableFeignClients(basePackages = {"study.springcloud.provider"})
-@SpringBootApplication
 public class Startup {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Startup.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(Startup.class, args);
+        SpringApplication.run(SpringBootCfg.class, args);
     }
 }
