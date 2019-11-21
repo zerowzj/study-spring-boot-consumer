fb0b7dc1c23207f21323fd1c5ce249065322fab9
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-21 17:25:35 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/consumer/support/SpringBootCfg.java
@@ -1,15 +1,10 @@
 package study.springcloud.consumer.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-//@EnableEurekaClient
-@EnableOAuth2Client
-@EnableResourceServer
+@EnableEurekaClient
 @EnableFeignClients(basePackages = {"study.springcloud.provider"})
 @SpringBootApplication(scanBasePackages = "study.springcloud.consumer")
 public class SpringBootCfg {
