cf199390ffafcd16d26b93194abc62feec1fc130
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-10 17:28:27 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/controller/EurekaController.java
+++ src/main/java/study/springcloud/consumer/controller/EurekaController.java
@@ -1,22 +1,35 @@
 package study.springcloud.consumer.controller;
 
 import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.DiscoveryClient;
 import com.netflix.discovery.EurekaClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/eureka")
+import java.util.List;
+
+@RestController
+@RequestMapping("/eureka")
 public class EurekaController {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(EurekaController.class);
+
     @Autowired
     private EurekaClient eurekaClient;
 
-    @PostMapping("")
-    public void demo() {
-        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("", false);
+    @PostMapping("/getNextServer")
+    public InstanceInfo getNextServer() {
+        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("study-springcloud-provider", false);
+        LOGGER.info("===>{}", instanceInfo);
+        return instanceInfo;
+    }
+
+    @PostMapping("/getInstances")
+    public List<InstanceInfo> getInstances(String serviceId) {
+        List<InstanceInfo> instanceInfoLt = eurekaClient.getInstancesById(serviceId);
+        return instanceInfoLt;
     }
 }
