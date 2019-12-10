8f7e1e540f898561c756ee767a74da498d481199
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-10 15:43:03 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/controller/EurekaController.java
+++ src/main/java/study/springcloud/consumer/controller/EurekaController.java
@@ -1,14 +1,22 @@
 package study.springcloud.consumer.controller;
 
+import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestControllerAdvice
+@RestController("/eureka")
 public class EurekaController {
 
+    @Autowired
     private DiscoveryClient discoveryClient;
+    @Autowired
+    private EurekaClient eurekaClient;
 
+    @PostMapping("")
     public void demo() {
-
+        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("", false);
     }
 }
