ae16f93ddf20d349194e103ef939f863e61250ff
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-11 11:37:43 +0800

    提交代码

--- src/main/java/study/springcloud/client/rest/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/client/rest/support/SpringBootCfg.java
@@ -2,12 +2,9 @@ package study.springcloud.client.rest.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@RibbonClient
 @EnableEurekaClient
-@EnableFeignClients(basePackages = {"study.springcloud.provider"})
 @SpringBootApplication(scanBasePackages = "study.springcloud.consumer")
 public class SpringBootCfg {
 }
