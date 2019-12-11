27e29dc7d496574a2e7e6ebbbc3f7948302e6271
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-11 16:44:31 +0800

    更新代码

--- src/main/java/study/springcloud/client/rest/controller/RestTemplateController.java
+++ src/main/java/study/springcloud/client/rest/controller/RestTemplateController.java
@@ -3,31 +3,33 @@ package study.springcloud.client.rest.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/rest")
 public class RestTemplateController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RestTemplateController.class);
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
 
-    @PostMapping("/ribbon")
-    public String ribbon() {
+    @PostMapping("/getServerInfo")
+    public String getServerInfo() {
         String str = restTemplate.postForObject("http://study-springcloud-provider/getServerInfo", null, String.class);
-//        String str = restTemplate.postForObject("http://study-springcloud-provider/getServerInfo", null, String.class);
         return str;
     }
 
-    @GetMapping("/sayHi")
-    public void sayHi() {
-        String str = restTemplate.getForObject("http://www.sohu.com", String.class);
-        System.out.println(str);
+    @PostMapping("/demo")
+    public String demo() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("study-springcloud-provider");
+        String str = restTemplate.postForObject("http://study-springcloud-provider/getServerInfo", null, String.class);
+        return str;
     }
 }
--- src/main/java/study/springcloud/client/rest/support/ribbon/RibbonCfg.java
+++ src/main/java/study/springcloud/client/rest/support/ribbon/RibbonCfg.java
@@ -2,16 +2,15 @@ package study.springcloud.client.rest.support.ribbon;
 
 import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.*;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RibbonClients(value = {
-        @RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
-//        ,@RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
-})
+//@RibbonClients(value = {
+//        @RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
+////        ,@RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
+//})
 public class RibbonCfg {
 
     @Bean
@@ -21,7 +20,8 @@ public class RibbonCfg {
 
     @Bean
     public IPing ribbonPing() {
-        return new PingUrl();
+//        return new PingUrl();
+        return new NoOpPing();
     }
 
 //    @Bean
