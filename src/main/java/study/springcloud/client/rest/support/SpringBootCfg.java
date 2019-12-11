36800c379548724b63a48ccce52b25138a3ea3f0
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-11 13:20:45 +0800

    更新代码

--- src/main/java/study/springcloud/client/rest/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/client/rest/support/SpringBootCfg.java
@@ -2,9 +2,10 @@ package study.springcloud.client.rest.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import study.springcloud.client.rest.support.ribbon.RibbonCfg;
 
-//@RibbonClient
 @EnableEurekaClient
-@SpringBootApplication(scanBasePackages = "study.springcloud.consumer")
+@SpringBootApplication(scanBasePackages = "study.springcloud.client.rest")
 public class SpringBootCfg {
 }
--- src/main/java/study/springcloud/client/rest/support/ribbon/RibbonCfg.java
+++ src/main/java/study/springcloud/client/rest/support/ribbon/RibbonCfg.java
@@ -2,10 +2,16 @@ package study.springcloud.client.rest.support.ribbon;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RibbonClients(value = {
+        @RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class),
+        @RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
+})
 public class RibbonCfg {
 
     @Bean
