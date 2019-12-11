3c9e90230114009c9576f71c4e345986ad89c3aa
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-11 11:15:37 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/support/ribbon/RibbonCfg.java
+++ src/main/java/study/springcloud/consumer/support/ribbon/RibbonCfg.java
@@ -1,12 +1,15 @@
 package study.springcloud.consumer.support.ribbon;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RibbonCfg {
 
-//    @Bean
-//    public IRule ribbonRule() {
-//        return new RandomRule();
-//    }
+    @Bean
+    public IRule ribbonRule() {
+        return new RandomRule();
+    }
 }
