6090f9904b2ea23066e1736e1f4eea70cba1ffb1
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-12 10:17:14 +0800

    更新代码

--- src/main/java/study/springcloud/client/rest/controller/TimeoutController.java
+++ src/main/java/study/springcloud/client/rest/controller/TimeoutController.java
@@ -2,6 +2,7 @@ package study.springcloud.client.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,8 +13,8 @@ public class TimeoutController {
     private RestTemplate restTemplate;
 
     @PostMapping("/await")
-    public String await() {
-        String str = restTemplate.postForObject("http://study-springcloud-provider/await", null, String.class);
+    public String await(@RequestParam Long timeout) {
+        String str = restTemplate.postForObject("http://study-springcloud-provider/await", null, String.class, timeout);
         return str;
     }
 }
