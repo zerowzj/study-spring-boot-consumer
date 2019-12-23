11fcd2a9e9875ba1320f40549cb48933d5912260
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-23 13:11:01 +0800

    更新代码

--- src/main/java/study/springcloud/client/rest/controller/RestTemplateController.java
+++ src/main/java/study/springcloud/client/rest/controller/RestTemplateController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +16,7 @@ public class RestTemplateController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RestTemplateController.class);
 
+    String url;
     @Autowired
     private RestTemplate restTemplate;
     @Autowired
@@ -26,15 +29,29 @@ public class RestTemplateController {
         return str;
     }
 
-    @PostMapping("/post")
-    public String post() {
-//        restTemplate.postForEntity()
+    @PostMapping("/postForEntity")
+    public String postForEntity() {
+        HttpHeaders headers = new Headers();
+        Class responseType = String.class;
+        ResponseEntity<String> response = restTemplate.postForEntity(url, null, responseType);
         return null;
     }
 
-    @PostMapping("/get")
-    public String get() {
-//        restTemplate.postForEntity()
+    @PostMapping("/postForObject")
+    public String postForObject() {
+        HttpHeaders headers = new Headers();
+        Class responseType = String.class;
+        String obj = restTemplate.postForObject(url, new String(), responseType);
+        return null;
+    }
+
+    @PostMapping("/getForEntity")
+    public String getForEntity() {
+        return null;
+    }
+
+    @PostMapping("/getForObject")
+    public String getForObject() {
         return null;
     }
 }
