9adcf195e1177d4b68406757feb4d52e9e8c6f97
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-07 17:57:16 +0800

    更新代码

--- src/main/java/study/springcloud/client/rest/support/rest/RestTemplateCfg.java
+++ src/main/java/study/springcloud/client/rest/support/rest/RestTemplateCfg.java
@@ -16,21 +16,42 @@ import java.time.Duration;
 @Configuration
 public class RestTemplateCfg {
 
+    private static final int DEFAULT_CONNECTION_TIMEOUT = 5 * 1000;
+
+    private static final int DEFAULT_READ_TIMEOUT = 5 * 1000;
+
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         RestTemplate restTemplate = builder.setConnectTimeout(Duration.ofSeconds(7))
                 .setReadTimeout(Duration.ofSeconds(7))
                 .build();
-        new SimpleClientHttpRequestFactory();
-        new OkHttp3ClientHttpRequestFactory();
-        new HttpComponentsClientHttpRequestFactory();
-//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-//        requestFactory.setReadTimeout(7 * 1000);
-//        requestFactory.setConnectTimeout(7 * 1000);
-//        RestTemplate restTemplate = new RestTemplate(requestFactory);
 
-//        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate;
+    }
+
+    public RestTemplate restTemplateByHttpCom1ponents() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(DEFAULT_CONNECTION_TIMEOUT);
+        factory.setConnectTimeout(DEFAULT_READ_TIMEOUT);
+        RestTemplate restTemplate = new RestTemplate(factory);
+        return restTemplate;
+    }
+
+    public RestTemplate restTemplateByHttpComponents() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(DEFAULT_CONNECTION_TIMEOUT);
+        factory.setConnectTimeout(DEFAULT_READ_TIMEOUT);
+        RestTemplate restTemplate = new RestTemplate(factory);
+        return restTemplate;
+    }
+
+    public RestTemplate restTemplateByOkHttp3() {
+        OkHttp3ClientHttpRequestFactory factory = new OkHttp3ClientHttpRequestFactory();
+        factory.setReadTimeout(DEFAULT_CONNECTION_TIMEOUT);
+        factory.setConnectTimeout(DEFAULT_READ_TIMEOUT);
+        RestTemplate restTemplate = new RestTemplate(factory);
         return restTemplate;
     }
 }
