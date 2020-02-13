0164c62282da296ab682a8e21b10bc7d563b8800
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-13 22:04:57 +0800

    更新代码

--- src/main/java/study/springcloud/client/rest/controller/DemoController.java
+++ src/main/java/study/springcloud/client/rest/controller/DemoController.java
@@ -3,10 +3,12 @@ package study.springcloud.client.rest.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import study.springcloud.client.rest.support.utils.Results;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
@@ -21,7 +23,16 @@ public class DemoController {
     @PostMapping("/throwException")
     public void throwException() {
         if (1 == 1) {
-            throw new RuntimeException("");
+            throw new RuntimeException("client exception");
+        }
+    }
+
+    @PostMapping("/timeout")
+    public void timeout(@RequestParam Long timeout) {
+        try {
+            TimeUnit.SECONDS.sleep(timeout);
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
