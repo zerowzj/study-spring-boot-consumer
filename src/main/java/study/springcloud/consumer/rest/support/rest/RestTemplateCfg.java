832898854a3f06cb77d822cd142b2a7a3dfb01bf
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-23 17:07:57 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/support/rest/RestTemplateCfg.java
+++ src/main/java/study/springcloud/consumer/rest/support/rest/RestTemplateCfg.java
@@ -5,26 +5,36 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
 import java.util.Collections;
 
 @Slf4j
 @Configuration
 public class RestTemplateCfg {
 
-    private static final int DEFAULT_CONNECTION_TIMEOUT = 5 * 1000;
-
-    private static final int DEFAULT_READ_TIMEOUT = 5 * 1000;
-
-    @Bean
+        @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-//        RestTemplate restTemplate = builder.setConnectTimeout(Duration.ofSeconds(7))
-//                .setReadTimeout(Duration.ofSeconds(7))
-//                .build()
+    public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setInterceptors(Collections.singletonList(new RestRequestInterceptor()));
+        ClientHttpRequestFactory requestFactory = restTemplate.getRequestFactory();
+        log.info(">>>>>> {}", requestFactory.getClass().getSimpleName());
+        return restTemplate;
+    }
+
+//    @Bean
+//    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        RestTemplate restTemplate = builder.setConnectTimeout(Duration.ofSeconds(7))
+                .setReadTimeout(Duration.ofSeconds(7))
+                .build();
+        restTemplate.setInterceptors(Collections.singletonList(new RestRequestInterceptor()));
+
+        ClientHttpRequestFactory requestFactory = restTemplate.getRequestFactory();
+        log.info(">>>>>> {}", requestFactory.getClass().getSimpleName());
         return restTemplate;
     }
 }
