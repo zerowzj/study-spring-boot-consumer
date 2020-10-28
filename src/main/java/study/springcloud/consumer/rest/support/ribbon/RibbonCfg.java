926207eb6ae093a4347d6dbc8102997329dc60e7
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-28 16:35:12 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/support/ribbon/RibbonCfg.java
+++ src/main/java/study/springcloud/consumer/rest/support/ribbon/RibbonCfg.java
@@ -1,43 +1,43 @@
-package study.springcloud.consumer.rest.support.ribbon;
-
-import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.*;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-
-@Slf4j
-//@Configuration
-//@RibbonClients(value = {
-//        @RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
-////        ,@RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
-//})
-public class RibbonCfg {
-
-    IClientConfig clientConfig;
-
-    @Bean
-    public IRule ribbonRule() {
-        new RoundRobinRule(); //轮询
-        new RetryRule(); //
-        IRule rule = new RandomRule();
-        return rule;
-    }
-
-
-    @Bean
-    public IPing ribbonPing() {
-        IPing ping = new NoOpPing();
-        return ping;
-    }
-
-//    @Bean
-//    public ServerList<Server> ribbonServerList() {
-//        return new ConfigurationBasedServerList();
+//package study.springcloud.consumer.rest.support.ribbon;
+//
+//import com.netflix.client.config.IClientConfig;
+//import com.netflix.loadbalancer.*;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Bean;
+//
+//@Slf4j
+////@Configuration
+////@RibbonClients(value = {
+////        @RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
+//////        ,@RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
+////})
+//public class RibbonCfg {
+//
+//    IClientConfig clientConfig;
+//
+////    @Bean
+//    public IRule ribbonRule() {
+//        new RoundRobinRule(); //轮询
+//        new RetryRule(); //
+//        IRule rule = new RandomRule();
+//        return rule;
 //    }
-
+//
+//
 //    @Bean
-//    public ServerListSubsetFilter serverListFilter() {
-//        ServerListSubsetFilter filter = new ServerListSubsetFilter();
-//        return filter;
+//    public IPing ribbonPing() {
+//        IPing ping = new NoOpPing();
+//        return ping;
 //    }
-}
+//
+////    @Bean
+////    public ServerList<Server> ribbonServerList() {
+////        return new ConfigurationBasedServerList();
+////    }
+//
+////    @Bean
+////    public ServerListSubsetFilter serverListFilter() {
+////        ServerListSubsetFilter filter = new ServerListSubsetFilter();
+////        return filter;
+////    }
+//}
