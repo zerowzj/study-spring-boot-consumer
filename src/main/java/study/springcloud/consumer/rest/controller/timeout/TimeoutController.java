b3fcb7ec3415f6285ef845ffd2eb2a6329054c29
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-30 09:53:45 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/controller/timeout/TimeoutController.java
+++ src/main/java/study/springcloud/consumer/rest/controller/timeout/TimeoutController.java
@@ -1,9 +1,7 @@
 package study.springcloud.consumer.rest.controller.timeout;
 
-import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +19,6 @@ public class TimeoutController {
 
     @RequestMapping("/timeout")
     public Map<String, Object> timeout(@RequestParam Long timeout) {
-        Map<String, Object> params = Maps.newHashMap();
-        params.put("timeout", timeout);
         String body = restTemplate.getForObject("http://study-springcloud-provider/timeout?timeout="+timeout, String.class);
         log.info(">>>>>> {}", body);
         return Results.ok();
