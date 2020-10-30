d5b07206718decd6fa6a482d29ce59cbea76ac84
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-30 13:26:43 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/consumer/rest/support/SpringBootCfg.java
@@ -1,13 +1,9 @@
 package study.springcloud.consumer.rest.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//服务注册和发现
 @EnableEurekaClient
-@EnableDiscoveryClient
-//
 @SpringBootApplication(scanBasePackages = "study.springcloud.consumer.rest")
 public class SpringBootCfg {
 }
