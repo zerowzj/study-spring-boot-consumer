7e57b1e1c505a343b327ad35bbb69f081fdf8c19
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-30 17:06:47 +0800

    更新代码

---
+++ src/main/java/study/springcloud/consumer/rest/controller/loadbalance/LoadBalanceController.java
@@ -0,0 +1,25 @@
+package study.springcloud.consumer.rest.controller.loadbalance;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import study.springcloud.consumer.rest.support.Results;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class LoadBalanceController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/getServerInfo")
+    public Map<String, Object> getServerInfo() {
+        Map<String, Object> data = restTemplate.getForObject("http://study-springcloud-provider/getServerInfo", Map.class);
+        log.info(">>>>>> {}", data);
+        return Results.ok();
+    }
+}
--- src/main/java/study/springcloud/consumer/rest/controller/retry/RetryController.java
+++ src/main/java/study/springcloud/consumer/rest/controller/retry/RetryController.java
@@ -2,62 +2,23 @@ package study.springcloud.consumer.rest.controller.retry;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import study.springcloud.consumer.rest.support.Results;
-
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
-@RequestMapping("/retry")
 public class RetryController {
 
     @Autowired
     private RestTemplate restTemplate;
-
-    @GetMapping("/timeout_get")
-    public Map<String, Object> timeout_get(@RequestParam Long timeout) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(timeout);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        Map<String, Object> data = Results.data();
-        data.put("timeout", String.format("%s ms", timeout));
-        return Results.ok(data);
-    }
-
-    @PostMapping("/timeout_post")
-    public Map<String, Object> timeout_post(@RequestParam Long timeout) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(timeout);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        Map<String, Object> data = Results.data();
-        data.put("timeout", String.format("%s ms", timeout));
-        return Results.ok(data);
-    }
-
-    @PostMapping("/timeout_idempotent")
-    public Map<String, Object> timeout_idempotent
-            (Long timeout, String orderNo) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(timeout);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        log.info("create order: [{}]", orderNo);
-        return Results.ok();
-    }
 
     //    @Retryable(maxAttempts = 6)
     @PostMapping("/retry")
