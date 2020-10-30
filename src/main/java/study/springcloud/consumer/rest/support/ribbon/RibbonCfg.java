cc203e69159778351d737b399a5f7f76ae48fb5d
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-30 13:15:56 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/support/ribbon/RibbonCfg.java
+++ src/main/java/study/springcloud/consumer/rest/support/ribbon/RibbonCfg.java
@@ -1,43 +1,52 @@
-//package study.springcloud.consumer.rest.support.ribbon;
-//
-//import com.netflix.client.config.IClientConfig;
-//import com.netflix.loadbalancer.*;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Bean;
-//
-//@Slf4j
-////@Configuration
-////@RibbonClients(value = {
-////        @RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
-//////        ,@RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
-////})
-//public class RibbonCfg {
-//
-//    IClientConfig clientConfig;
-//
-////    @Bean
-//    public IRule ribbonRule() {
-//        new RoundRobinRule(); //轮询
-//        new RetryRule(); //
-//        IRule rule = new RandomRule();
-//        return rule;
-//    }
-//
-//
+package study.springcloud.consumer.rest.support.ribbon;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.NoOpPing;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+//@RibbonClients(value = {
+//        @RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
+////        ,@RibbonClient(name = "study-springcloud-provider", configuration = RibbonCfg.class)
+//})
+//@RibbonClient()
+public class RibbonCfg {
+
+    private IClientConfig clientConfig;
+
+    //    @Bean
+    public IRule ribbonRule() {
+        new RoundRobinRule(); //轮询
+        new RetryRule(); //
+        IRule rule = new RandomRule();
+        return rule;
+    }
+
+
+    @Bean
+    public IPing ribbonPing() {
+        IPing ping = new NoOpPing();
+        return ping;
+    }
+
 //    @Bean
-//    public IPing ribbonPing() {
-//        IPing ping = new NoOpPing();
-//        return ping;
+//    public ServerList<Server> ribbonServerList() {
+//        return new ConfigurationBasedServerList();
 //    }
-//
-////    @Bean
-////    public ServerList<Server> ribbonServerList() {
-////        return new ConfigurationBasedServerList();
-////    }
-//
-////    @Bean
-////    public ServerListSubsetFilter serverListFilter() {
-////        ServerListSubsetFilter filter = new ServerListSubsetFilter();
-////        return filter;
-////    }
-//}
+
+//    @Bean
+//    public ServerListSubsetFilter serverListFilter() {
+//        ServerListSubsetFilter filter = new ServerListSubsetFilter();
+//        return filter;
+//    }
+}
