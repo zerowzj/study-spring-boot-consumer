811400217a772d009573677a7899abecd777cb24
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-30 17:39:22 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/consumer/rest/support/SpringBootCfg.java
@@ -2,9 +2,8 @@ package study.springcloud.consumer.rest.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.retry.annotation.EnableRetry;
 
-@EnableRetry
+//@EnableRetry
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "study.springcloud.consumer.rest")
 public class SpringBootCfg {
