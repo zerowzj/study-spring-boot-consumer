6f2a0a0e8a0370ae3f3d925f128d79419cb32754
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-05 13:46:20 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/support/rest/RestTemplateCfg.java
+++ src/main/java/study/springcloud/consumer/rest/support/rest/RestTemplateCfg.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
-
 @Slf4j
 @Configuration
 public class RestTemplateCfg {
@@ -23,5 +21,4 @@ public class RestTemplateCfg {
                 .build();
         return restTemplate;
     }
-
 }
