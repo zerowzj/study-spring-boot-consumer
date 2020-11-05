946610d723f80cf48862b065edefb7b263ce91b4
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-05 16:46:38 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/consumer/rest/support/SpringBootCfg.java
@@ -1,9 +1,12 @@
 package study.springcloud.consumer.rest.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
-@SpringBootApplication(scanBasePackages = "study.springcloud.consumer.rest")
+@SpringBootApplication(scanBasePackages = "study.springcloud.consumer.rest",
+        exclude = {})
 public class SpringBootCfg {
 }
