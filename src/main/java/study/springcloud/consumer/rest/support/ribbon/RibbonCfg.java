3b80ece8ee119fe8c76b1b528059c3a4d214c13c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-06 09:49:42 +0800

    更新代码

--- src/main/java/study/springcloud/consumer/rest/support/ribbon/RibbonCfg.java
+++ src/main/java/study/springcloud/consumer/rest/support/ribbon/RibbonCfg.java
@@ -6,26 +6,29 @@ import com.netflix.loadbalancer.NoOpPing;
 import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.RoundRobinRule;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
+@RibbonClient(name = "study-springcloud-provider")
 public class RibbonCfg {
 
-    @Bean
-    public IRule ribbonRule() {
-        IRule rule = new RoundRobinRule();
-//        IRule rule = new RandomRule();
-        return rule;
-    }
+//    @Bean
+//    public IRule ribbonRule() {
+//        IRule rule = new RoundRobinRule();
+////        IRule rule = new RandomRule();
+//        return rule;
+//    }
 
 
 //    @Bean
-    public IPing ribbonPing() {
-        IPing ping = new NoOpPing();
-        return ping;
-    }
+//    public IPing ribbonPing() {
+//        IPing ping = new NoOpPing();
+//        return ping;
+//    }
 
 //    @Bean
 //    public ServerList<Server> ribbonServerList() {
